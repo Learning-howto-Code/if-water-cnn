7dee89d8afc2fafc667fe0073dd45e21ca423f9e
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-11-23 10:17:37 -0800

    inference now ses .tflite model

--- deploy.py
+++ deploy.py
@@ -1,32 +1,45 @@
-from tensorflow import keras
 from picamera2 import Picamera2
-from time import sleep, strftime, time
-import os
+from time import sleep
+import numpy as np
 import cv2
-# loads model
-model = keras.models.load_model("model.keras")
-# starts camera as a video so it can go faster
+import tflite_runtime.interpreter as tflite
+
+
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+
+h = input_details[0]["shape"][1]
+w = input_details[0]["shape"][2]
+
+#sets up camera 
 picam2 = Picamera2()
-config = picam2.create_video_configuration(main={"size": (224, 224)}, buffer_count=4)
+config = picam2.create_video_configuration(main={"size": (w, h)}, buffer_count=4)
 picam2.configure(config)
 picam2.start()
 
 
-
 def test_pic():
     frame = picam2.capture_array()
-    img = cv2.resize(frame, (224, 224))
-    img = img / 255.0
-    img = img.reshape(1, 224, 224, 3)
-    prediction = model.predict(img)
+
+    # Resize + normalize
+    img = cv2.resize(frame, (w, h))
+    img = img.astype(np.float32) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    # Send input to TFLite model
+    interpreter.set_tensor(input_details[0]["index"], img)
+    interpreter.invoke()
+
+    # Read output
+    prediction = interpreter.get_tensor(output_details[0]["index"])
     print(prediction)
     return prediction
-
-while True:
+x = 5
+while x < 5:
     sleep(0.2)
     test_pic()
-#steps
-
-#take an image
-#pass that image into the model
-#delete the image
+    x += 1
