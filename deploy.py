6aba4928cad82de918df1806d470834a0943e6f4
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-11-24 16:46:24 -0800

    testing

--- deploy.py
+++ deploy.py
@@ -28,7 +28,7 @@ def test_pic():
     # Resize + normalize
     img = cv2.resize(frame, (w, h))
     img = img.astype(np.float32) / 255.0
-      img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=0)
     interpreter.set_tensor(input_details[0]["index"], img)
     interpreter.invoke()
 
